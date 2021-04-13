e29ab29ee4191ba29965582092f3b5369c441463
Terry <1276400081@qq.com>
2021-04-13 14:56:30 +0800

    增加批量生成用例脚本

--- bin/caseCreateAndRun.py
+++ bin/caseCreateAndRun.py
@@ -2,6 +2,7 @@ import sys
 import os
 from settings import *
 from lib.create_cases import case_create
+from lib.fileGet import FileUtils
 
 #将当前项目的目录加入零时环境变量，避免在其他地方运行时会出现引入错误
 base_path = os.path.dirname(
@@ -21,17 +22,41 @@ foldname = "Position"
 #yml文件的二级目录为
 
 def runCase(file:str, template_file = templateFile, path=CASE_PATH) ->bool:
+    """
+
+    :param file:
+    :param template_file:
+    :param path: 执行用例的目录
+    :return:
+    """
     case_create(file=file, template_file=template_file)
     caseName = file.split(".")[0]
     print("执行用例:{}_{}".format("test", caseName))
     file = os.path.join(path, "test_" + caseName + ".py")
     os.system("pytest" + " " + "-sv" + " " + file)
 
-if __name__ == '__main__':
-    # 首先调用生成用例函数
-    # create_case_file(_path=path, files="deleteEducations.yaml") #指定路径，创建指定文件的测试代码
-    #case_create(file="rentin.yaml", template_file=templateFile)
-    runCase(file="xx.yaml", template_file=templateFile)
+def batchRunCase(dataPath:str, template_file = templateFile, path=CASE_PATH):
+    """
 
+    :param dataPath: 批量生成case的目录
+    :param template_file:
+    :param path:
+    :return:
+    """
+    if FileUtils.isexists(dataPath):
+        try:
+            file_lists = os.listdir(dataPath)  # 取出data目录下的所有文件
+            for file in file_lists:
+                runCase(file=file, template_file=template_file, path=path)
+        except Exception as e:
+            print("批量生成case失败，原因是:{}".format(e))
+    else:
+        raise Exception("当前路径不存在")
+
+
+if __name__ == '__main__':
+    # runCase(file="rentin.yaml", template_file=templateFile)
+    # runCase(file="bill.yaml", template_file=templateFile)
+    batchRunCase(DATA_PATH, templateFile)
 
 
